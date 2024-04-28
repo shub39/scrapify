6d2a49e0ee4de7b9d97b834367ca05387af965a5
Shubham <cptnshubham39@gmail.com>
2024-04-28 14:58:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import random
 from dotenv import load_dotenv
 import discord
-from discord.ext import commands
+import requests
+from discord.ext import commands, tasks
 import scraper
 
 load_dotenv()
@@ -11,44 +13,81 @@ intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-tree = bot.tree
-url = 'https://www.makautexam.net/announcement.html'
-csv_file_path = 'data.csv'
-
-@tree.command(name="sylvian", description="Summon Sylvian!")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message("Sylvian severed ties with humanity long ago, yet her presence continues to permeate the world. In Fear & Hunger, an unique encounter known as Traces of Sylvian serves as a manifestation of her lingering essence.")
-
-@bot.event
-async def on_ready():
-    print("Sylvian's presence is strong.")
-    await tree.sync()
+makaut_url = 'https://www.makautexam.net/announcement.html'
+makaut_path = 'makaut_anchors.csv'
 
 @bot.command(name="purge")
-async def purge(ctx, amount: int):
+async def purge(ctx, messages: int):
     if ctx.author.guild_permissions.manage_messages:
-        await ctx.channel.purge(limit=amount + 1)  
-        await ctx.send(f"Deleted {amount} messages.")
+        await ctx.channel.purge(limit=messages + 1)
+        await ctx.send(f"Deleted {messages} messages.")
     else:
         await ctx.send("You do not have permission to use this command.")
 
+@bot.command(name='coin')
+async def flipcoin(ctx):
+    result = random.choice(['Heads', 'Tails'])
+    await ctx.send(f"The coin landed on: **{result}**")
+
+@bot.command(name='roll')
+async def roll(ctx):
+    result = random.randint(1, 6)
+    await ctx.send(f"The dice rolled: **{result}**")
+
+@bot.command(name='fetch')
+async def student_details(ctx, name: str):
+    student = scraper.get_student_details(name)
+    if student:
+        message = "**Student details found:**\n"
+        for key, value in student.items():
+            message += f"**{key}**: {value}\n"
+        await ctx.send(message)
+    else:
+        await ctx.send("Student not found.")
+
 @bot.command(name="makaut")
 async def update_data(ctx):
-    new_data = scraper.fetch_data_from_website(url)
-    previous_data = scraper.read_data_from_csv(csv_file_path)
+    new_data = scraper.fetch_data_from_website(makaut_url)
+    previous_data = scraper.read_data_from_csv(makaut_path)
     new_links = [link for link in new_data if link not in previous_data]
     new_content = [(link, new_data[link]) for link in new_links]
-    scraper.write_data_to_csv(csv_file_path, new_data)
+    scraper.write_data_to_csv(makaut_path, new_data)
     if not new_links:
         await ctx.send("No new links.")
     else:
-        message = f"Scraping makaut's website: {makaut}\n"
+        message = f"Scraping makaut's website: {makaut_url}\n"
         message += "New links and content:\n"
         for link, content in new_content:
             message += f"Content: **{content}**\n"
         await ctx.send(message)
 
+@bot.command(name='quote')
+async def quote(ctx):
+    response = requests.get('https://api.quotable.io/random')
+    if response.status_code == 200:
+        data = response.json()
+        quote = data['content']
+        await ctx.send(f"Quote: {quote}")
+    else:
+        await ctx.send("Failed to fetch a quote.")
 
-bot_token = os.getenv("BOT_TOKEN")
+@tasks.loop(hours=6)
+async def scheduled_task():
+    channel = discord.utils.get(bot.get_all_channels(), name='makaut-updates')
+    new_data = scraper.fetch_data_from_website(makaut_url)
+    previous_data = scraper.read_data_from_csv(makaut_path)
+    new_links = [link for link in new_data if link not in previous_data]
+    new_content = [(link, new_data[link]) for link in new_links]
+    scraper.write_data_to_csv(makaut_path, new_data)
+    if new_links:
+        message = f"Scraping makaut's website: {makaut_url}\n"
+        message += "New links and content:\n"
+        for link, content in new_content:
+            message += f"Content: **{content}**\n"
+        await channel.send(message)
 
-bot.run(bot_token)
+@scheduled_task.before_loop
+async def before_scheduled_task():
+    await bot.wait_until_ready()
+
+bot.run(os.getenv("BOT_TOKEN"))
