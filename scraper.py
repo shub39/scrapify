10fbc2bc299552b72d9e037ae4d2440c4ea8ea4d
Shubham <cptnshubham39@gmail.com>
2024-04-28 14:59:45 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,6 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import time
+import pyexcel
+
+
+def get_student_details(name):
+    try:
+        data = pyexcel.get_sheet(file_name='DS_DATA.ods')
+        name_column_index = None
+        for index, cell_value in enumerate(data.row[0]):
+            if cell_value.lower() == 'name':
+                name_column_index = index
+                break
+        if name_column_index is None:
+            return None  
+        for row_index, row in enumerate(data):
+            if row_index == 0:  
+                continue
+            if row[name_column_index].lower() == name.lower():
+                student_details = {}
+                for col_index, col_name in enumerate(data.row[0]):
+                    student_details[col_name] = row[col_index]
+                return student_details        
+                return None 
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 def fetch_data_from_website(url):
     response = requests.get(url)
